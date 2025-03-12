6be15ea9408ea59a58f512f318d644d12b24bf45
Anna Xu <77125837+annaxu9@users.noreply.github.com>
2025-03-12 15:34:52 -0400

    took out red and added sky blue to color picker

--- frontend/components/ui/color-picker.tsx
+++ frontend/components/ui/color-picker.tsx
@@ -1,13 +1,17 @@
-'use client';
+"use client";
 
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
-import { useStore } from '@/lib/hooks';
-import type { GraphStore } from '@/lib/interface';
-import { cn } from '@/lib/utils';
-import { Paintbrush } from 'lucide-react';
-import React from 'react';
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { useStore } from "@/lib/hooks";
+import type { GraphStore } from "@/lib/interface";
+import { cn } from "@/lib/utils";
+import { Paintbrush } from "lucide-react";
+import React from "react";
 
 export function ColorPicker({
   color,
@@ -18,12 +22,25 @@ export function ColorPicker({
   property: keyof GraphStore;
   className?: string;
 }) {
-  const solids = ['black', 'hotpink', 'orange', 'yellow', 'limegreen', 'aquamarine', 'darkorchid', 'red', 'blue'];
+  const solids = [
+    "black",
+    "hotpink",
+    "orange",
+    "yellow",
+    "limegreen",
+    "aquamarine",
+    "skyblue",
+    "darkorchid",
+    "blue",
+  ];
 
-  const handleNodeColorChange = (e: React.KeyboardEvent<HTMLInputElement> | string, key: keyof GraphStore) => {
-    if (typeof e === 'string') {
+  const handleNodeColorChange = (
+    e: React.KeyboardEvent<HTMLInputElement> | string,
+    key: keyof GraphStore
+  ) => {
+    if (typeof e === "string") {
       useStore.setState({ [key]: e });
-    } else if (e.key === 'Enter') {
+    } else if (e.key === "Enter") {
       useStore.setState({ [key]: e.currentTarget.value });
     }
   };
@@ -34,38 +51,47 @@ export function ColorPicker({
     <Popover>
       <PopoverTrigger asChild>
         <Button
-          variant={'outline'}
-          className={cn('w-[220px] justify-start text-left font-normal', !color && 'text-muted-foreground', className)}
+          variant={"outline"}
+          className={cn(
+            "w-[220px] justify-start text-left font-normal",
+            !color && "text-muted-foreground",
+            className
+          )}
         >
-          <div className='w-full flex items-center gap-2'>
+          <div className="w-full flex items-center gap-2">
             {color ? (
-              <div className='h-4 w-4 rounded !bg-center !bg-cover transition-all' style={{ background: color }} />
+              <div
+                className="h-4 w-4 rounded !bg-center !bg-cover transition-all"
+                style={{ background: color }}
+              />
             ) : (
-              <Paintbrush className='h-4 w-4' />
+              <Paintbrush className="h-4 w-4" />
             )}
-            <span className='truncate flex-1'>{color ? color : 'Pick a color'}</span>
+            <span className="truncate flex-1">
+              {color ? color : "Pick a color"}
+            </span>
           </div>
         </Button>
       </PopoverTrigger>
-      <PopoverContent className='w-36 md:w-64' align='end'>
-        <div className='flex flex-wrap'>
-          {solids.map(s => (
+      <PopoverContent className="w-36 md:w-64" align="end">
+        <div className="flex flex-wrap">
+          {solids.map((s) => (
             // biome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
             <div
               key={s}
               style={{ background: s }}
-              onClick={e => handleNodeColorChange(s, property)}
-              className='rounded-md h-6 w-6 cursor-pointer hover:scale-105'
+              onClick={(e) => handleNodeColorChange(s, property)}
+              className="rounded-md h-6 w-6 cursor-pointer hover:scale-105"
             />
           ))}
         </div>
 
         <Input
-          id='custom'
+          id="custom"
           value={inputValue}
-          className='col-span-2 h-8 mt-4'
-          onChange={e => setInputValue(e.target.value)}
-          onKeyDown={e => handleNodeColorChange(e, property)}
+          className="col-span-2 h-8 mt-4"
+          onChange={(e) => setInputValue(e.target.value)}
+          onKeyDown={(e) => handleNodeColorChange(e, property)}
         />
       </PopoverContent>
     </Popover>

fe2b80097e4d3a295441fcf8f6ad3fc9cb4a2ebd
Anna Xu <77125837+annaxu9@users.noreply.github.com>
2025-04-08 23:48:43 -0400

    fix linter

--- frontend/components/ui/combobox.tsx
+++ frontend/components/ui/combobox.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Check, ChevronsUpDown, Info } from 'lucide-react';
+import { ChevronsUpDown, Info } from 'lucide-react';
 import * as React from 'react';
 
 import { Button } from '@/components/ui/button';

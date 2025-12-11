c00534b0dea795f4f9ac05fd6f6e7f13f5906616
sajalgarg035 <sajalgargofficial@gmail.com>
2025-12-11 23:02:52 +0530

    feat(data-commons): add documentation for analysis modules

---
+++ backend/cron/index.ts
@@ -0,0 +1,8 @@
+import cron from "node-cron";
+import { cleanupOldData } from "./pg_cleanup_trigger";
+
+// Run 4 times a Day
+cron.schedule("0 */6 * * *", async () => {
+  console.log("[CRON] Starting cleanup job...");
+  await cleanupOldData();
+});

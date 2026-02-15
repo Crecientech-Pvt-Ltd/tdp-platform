0dedc0ab8c468057def4efcec784c0e50f61bc9e
23abdul23 <23abdulazeem23@gmail.com>
2026-02-15 11:12:58 +0530

    fix: docker build fixed

--- frontend/nginx/entrypoint.sh
+++ frontend/nginx/entrypoint.sh
@@ -1,24 +1,4 @@
 #!/bin/sh
 
-set -e
-
-# Ensure AUTH_MAP is set
-if [ -z "$AUTH_MAP" ]; then
-    echo "AUTH_MAP is not set. Editing nginx.conf to remove basic auth."
-    sed -i 's/auth_basic/#auth_basic/g' /etc/nginx/conf.d/default.conf
-else
-    # Clear the existing .htpasswd file
-    > /etc/nginx/.htpasswd
-
-    # Set IFS to ';' and split AUTH_MAP into key-value pairs
-    IFS=';'
-    for pair in $AUTH_MAP; do
-        user=$(echo $pair | cut -d= -f1)
-        password=$(echo $pair | cut -d= -f2)
-        htpasswd -bB /etc/nginx/.htpasswd $user $password
-    done
-    echo "Updated .htpasswd file"
-fi
-
 # Start Nginx
 nginx -g "daemon off;"
